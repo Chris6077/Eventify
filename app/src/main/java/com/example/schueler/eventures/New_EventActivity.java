d3059269ccc6a8b2eca09a13d9ba09fae4f299c6
vanessa000 <julian.blaschke@gmx.at>
2018-03-16 13:04:49 -0700

    event add / participants

---
+++ app/src/main/java/com/example/schueler/eventures/New_EventActivity.java
@@ -0,0 +1,15 @@
+package com.example.schueler.eventures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.schueler.eventures.R;
+
+public class New_EventActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new__event);
+	}
+}
