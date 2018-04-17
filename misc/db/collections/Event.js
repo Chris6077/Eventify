2ec3df2dc88900e4ff6c826ea7d1b62469b89f21
Christian Winkler <chris@bashit.me>
2018-04-17 11:45:23 +0200

    Added Event Schema / fixed Event in CD

---
+++ misc/db/collections/Event.js
@@ -0,0 +1,56 @@
+db.createCollection("Events", {
+   validator: {
+      $jsonSchema: {
+         bsonType: "object",
+         required: [ "name", "state", "description", "maxParticipants", "minAge", "type", "startDate", "endDate", "created", "lastEdited" ],
+         properties: {
+            name: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            state: {
+               enum: ["Unconfirmed", "Confirmed", "CalledOff"],
+               description: "can only be one of the enum values and is required"
+            },
+            description: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            maxParticipants: {
+               bsonType: "int",
+               minimum: 1,
+               maximum: 2000000000,
+               exclusiveMaximum: false,
+               description: "must be an integer in [ 0, 2000000000 ] and is required"
+            },
+            minAge: {
+               bsonType: "int",
+               minimum: 0,
+               maximum: 150,
+               exclusiveMaximum: false,
+               description: "must be an integer in [ 0, 150 ] and is required"
+            },
+            type: {
+               enum: ["Public", "NoList", "Private"],
+               description: "can only be one of the enum values and is required"
+            },
+            startDate: {
+               bsonType: "date",
+               description: "must be a date and is required"
+            },
+            endDate: {
+               bsonType: "date",
+               description: "must be a date and is required"
+            },
+            created: {
+               bsonType: "date",
+               description: "must be a date and is required"
+            },
+            lastEdited: {
+               bsonType: "date",
+               description: "must be a date and is required"
+            }
+         }
+      }
+   }
+})
