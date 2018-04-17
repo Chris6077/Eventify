53bc0ae3b224c6627ded631e7865008aaa427306
Christian Winkler <chris@bashit.me>
2018-04-17 13:54:51 +0200

    Finished Participate Schema

---
+++ misc/db/collections/JSONSchemas/Participate.js
@@ -0,0 +1,23 @@
+{
+   "bsonType": "object",
+   "required": [ "UserID", "EventID", "type", "created" ],
+   "properties": {
+      "UserID": {
+         "bsonType": "string",
+         "description": "must be a string and is required"
+      },
+      "EventID": {
+         "bsonType": "string",
+         "description": "must be a string and is required"
+      },
+      "type": {
+         "enum": ["Public", "Anonymous"],
+         "description": "can only be one of the enum values and is required"
+      },
+      "created": {
+         "bsonType": "date",
+         "description": "must be a date and is required"
+      }
+   },
+   "additionalProperties": false
+}
