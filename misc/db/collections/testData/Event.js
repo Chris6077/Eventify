6f49131dd5a80079dccfa8a8af62ca6782f44654
Christian Winkler <chris@bashit.me>
2018-04-17 12:30:12 +0200

    Add original JSON Schema / Add Event TestObject

---
+++ misc/db/collections/testData/Event.js
@@ -0,0 +1,14 @@
+//'Content-Type: application/json'
+{
+    "name": "Event1",
+    "state": "Confirmed",
+    "description": "TestEvent1",
+    "maxParticipants": 20,
+    "minAge": 18,
+    "type": "Public",
+    "startDate": "2018-04-17T10:28:39.535Z",
+    "endDate": "2018-04-17T10:28:39.535Z",
+    "created": "2018-04-17T10:28:39.535Z",
+    "lastEdited": "2018-04-17T10:28:39.535Z",
+    "location": { "lat": 1.454 , "lon": 2.536 }
+}
