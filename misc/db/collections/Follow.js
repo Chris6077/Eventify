674a213bbed27180c41ad1c5e4c7c9252f18b0d3
Christian Winkler <chris@bashit.me>
2018-04-17 13:45:50 +0200

    Finished Follow Schema

---
+++ misc/db/collections/Follow.js
@@ -0,0 +1,23 @@
+db.createCollection("Follows", {
+   validator: {
+      $jsonSchema: {
+         bsonType: "object",
+         required: [ "User1ID", "User2ID", "created"],
+         properties: {
+            User1ID: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            User2ID: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            created: {
+               bsonType: "date",
+               description: "must be a date and is required"
+            }
+         },
+         additionalProperties: false
+      }
+   }
+})
