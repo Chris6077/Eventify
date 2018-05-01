af04120f93af0eced1547549876fe7c1ee64c468
vanessa000 <julian.blaschke@gmx.at>
2018-05-01 04:51:06 -0700

    .

--- app/src/main/java/com/example/schueler/eventures/UserActivity.java
+++ app/src/main/java/com/example/schueler/eventures/UserActivity.java
@@ -2,6 +2,10 @@ package com.example.schueler.eventures;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -13,11 +17,17 @@ import android.widget.ImageView;
 
 import com.example.schueler.eventures.asynctask.DownloadImageTask;
 
+import java.io.IOException;
+
 public class UserActivity extends AppCompatActivity {
 
 	private CollapsingToolbarLayout collapsingToolbar;
 	private View logoutView;
+	private FloatingActionButton fab_choose_pic;
+	private int PICK_IMAGE_REQUEST = 1;
 
+
+	//super
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,15 +39,37 @@ public class UserActivity extends AppCompatActivity {
 
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+			Uri uri = data.getData();
+
+			try {
+				Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+				// Log.d(TAG, String.valueOf(bitmap));
+
+				ImageView imageView = (ImageView) findViewById(R.id.User_header_img);
+				imageView.setImageBitmap(bitmap);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	//custom
 
 	private void setViews(){
 		this.collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar_user);
 		this.logoutView = findViewById(R.id.list_item_user_logout);
+		this.fab_choose_pic = (FloatingActionButton) findViewById(R.id.fab_choose_picture_user);
 	}
 
 	private void registrateeventhandlers(){
 		this.logoutView.setOnClickListener(new logout_listener());
+		this.fab_choose_pic.setOnClickListener(new choose_picture_listener());
 	}
 
 	private void setContent(){
@@ -61,6 +93,19 @@ public class UserActivity extends AppCompatActivity {
 			user_activity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
 			startActivity(user_activity);
 
+		}
+	}
+
+	private class choose_picture_listener implements View.OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent();
+			// Show only images, no videos or anything else
+			intent.setType("image/*");
+			intent.setAction(Intent.ACTION_GET_CONTENT);
+			// Always show the chooser (if there are multiple options available)
+			startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
 		}
 	}
 
