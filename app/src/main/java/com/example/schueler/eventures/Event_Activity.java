5e0c9a97e96f029b6f9a6334941db4209abec5be
vanessa000 <julian.blaschke@gmx.at>
2018-05-03 23:31:31 -0700

    .

--- app/src/main/java/com/example/schueler/eventures/Event_Activity.java
+++ app/src/main/java/com/example/schueler/eventures/Event_Activity.java
@@ -3,6 +3,7 @@ package com.example.schueler.eventures;
 import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
@@ -11,6 +12,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Pair;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
@@ -19,7 +21,22 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.schueler.eventures.adapter.adapter_list_view_event;
 import com.example.schueler.eventures.asynctask.DownloadImageTask;
+import com.example.schueler.eventures.classes.pojo.Event;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Event_Activity extends AppCompatActivity implements View.OnClickListener {
 
@@ -113,6 +130,23 @@ public class Event_Activity extends AppCompatActivity implements View.OnClickLis
 
 	}
 
+	private void setInfoItem(String header, String description, int pictureResource, View.OnClickListener listener){
+		LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View item = inflater.inflate(R.layout.listview_item_event,null, false);
+
+		TextView head = (TextView) item.findViewById(R.id.cardiview_info_item_header_text);
+		TextView desc = (TextView) item.findViewById(R.id.cardiview_info_item_description);
+		ImageView image = (ImageView) item.findViewById(R.id.cardiview_info_item_image);
+
+		head.setText(header);
+		desc.setText(description);
+		image.setImageResource(pictureResource);
+
+		item.setOnClickListener(listener);
+
+		this.content_event.addView(item);
+	}
+
 	//super
 
 	@Override
@@ -130,7 +164,92 @@ public class Event_Activity extends AppCompatActivity implements View.OnClickLis
 
 	//async Task
 
+	private class LoadEventsSync extends AsyncTask<Object, Object, ArrayList<Event>> {
 
+		//fields
+		private String url;
+
+		//constructors
+		public LoadEventsSync(String url) {
+			this.url = url;
+		}
+
+
+		//super
+		@Override
+		protected ArrayList<Event> doInBackground(Object... params) {
+			try {
+				Gson gson = new Gson();
+				HttpURLConnection conn = (HttpURLConnection) new URL(this.url + "getAllEvents").openConnection();
+				Type collectionType = new TypeToken<Collection<Event>>(){}.getType();
+				String result = GetData(conn);
+				ArrayList<Event> events = gson.fromJson(result, collectionType);
+				return events;
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(ArrayList<Event> events){
+			super.onPostExecute(events);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+
+		private void PostData(HttpURLConnection conn, String... params){
+			BufferedWriter writer;
+
+			try{
+
+				//posting the data
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/json");
+				writer = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+				writer.write(params[0]); //product - object in json-format
+				writer.flush();
+				writer.close();
+				conn.getResponseCode();
+
+			}catch(Exception error){
+				System.out.println("ERROR --- " + error);
+			}
+
+		}
+
+		private String GetData(HttpURLConnection conn){
+			BufferedReader reader;
+			String content = null;
+
+			try{
+				//reading the result
+
+				reader = new BufferedReader(new InputStreamReader(
+						conn.getInputStream()));
+				StringBuilder sb = new StringBuilder();
+				String line;
+
+				while ((line = reader.readLine()) != null) {
+					sb.append(line);
+				}
+
+				content = sb.toString();
+				reader.close();
+				conn.disconnect();
+
+
+			}catch(Exception error){
+				System.out.println("ERROR --- " + error);
+			}
+			return content;
+		}
+
+	}
 
 
 }
